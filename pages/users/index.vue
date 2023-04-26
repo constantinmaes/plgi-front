c01c1ee75296d104e6a5f6c5a25198399c93a31b
Maes Constantin <constantinmaes@gmail.com>
2023-04-26 18:34:10 +0200

    wip

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,20 +1,45 @@
 <template>
+    <Dialog v-model:visible="visible">
+        <div v-if="user">
+            <p>User {{ user.email }}</p>
+        </div>
+        <div v-else>
+            <Skeleton width="10rem"></Skeleton>
+        </div>
+    </Dialog>
     <DataTable :value="users">
         <Column field="name" header="Name" sortable></Column>
         <Column field="email" header="Email address" sortable></Column>
         <Column field="age" header="Age"></Column>
         <Column field="_id">
             <template #body="{ data }">
-                <NuxtLink :to="'/users/' + data._id">Voir</NuxtLink>
+                <!--<NuxtLink :to="'/users/' + data._id">Voir</NuxtLink>-->
+                <span @click="toggleDialog(data._id)">Voir</span>
             </template>
         </Column>
     </DataTable>
 </template>
 
 <script setup lang="ts">
+import { User } from '~/types';
 import $http from '~/utils/http';
 
+const user = ref<User>();
 const users = ref([]);
+const visible = ref(false);
+
+const toggleDialog = async (_id: string) => {
+    if (!visible.value) {
+        setTimeout(async () => {
+            const { data } = await $http.get(`/users/${_id}`);
+            user.value = data;
+        }, 2000);
+    } else {
+        user.value = undefined;
+    }
+
+    visible.value = !visible.value;
+};
 
 onMounted(async () => {
     const { data } = await $http.get('/users');
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -2,12 +2,16 @@ import { defineNuxtPlugin } from '#app';
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
 import DataTable from 'primevue/datatable';
+import Dialog from 'primevue/dialog';
 import Column from 'primevue/column';
+import Skeleton from 'primevue/skeleton';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: true });
     nuxtApp.vueApp.component('Button', Button);
     nuxtApp.vueApp.component('DataTable', DataTable);
+    nuxtApp.vueApp.component('Dialog', Dialog);
     nuxtApp.vueApp.component('Column', Column);
+    nuxtApp.vueApp.component('Skeleton', Skeleton);
     //other components that you need
 });
